90eb0fbd696763bb9adcb51f5286ee72614af60b
Osigelialex <osigelialex@gmail.com>
2025-04-12 22:27:41 +0100

    Updated baseURL to new backend url

--- lib/axiosConfig.ts
+++ lib/axiosConfig.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  // baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://checkmate-theta.vercel.app/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
